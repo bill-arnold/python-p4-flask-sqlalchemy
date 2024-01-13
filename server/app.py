4c374f0dc9ca1c48800ed42570e9cff022894402
bill <bill.arnold@student.moringaschool.com>
2024-01-13 14:09:49 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from models import db
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-
-#migrate=Migrate(app,db)
-#db.init_app(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Create a manager instance for handling commands
+manager = Manager(app)
+
+# Add the 'db' command to the manager
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    # Run the manager instead of the app
+    manager.run()
--- server/models.py
+++ server/models.py
@@ -1,22 +1,18 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy import MetaData
-
-#metadata = MetaData(naming_convention={
-    #"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-#})
 
 db = SQLAlchemy()
 
-class Owner(db.model):
-    __tablename__ = 'owner'
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
+
     pets = db.relationship('Pet', backref='owner')
 
     def __repr__(self):
         return f'<Pet Owner {self.name}>'
-    
+
 class Pet(db.Model):
     __tablename__ = 'pets'
 
@@ -27,6 +23,4 @@ class Pet(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
 
     def __repr__(self):
-        return f'<Pet {self.name}, {self.species}>
-
-#db = SQLAlchemy(metadata=metadata)
+        return f'<Pet {self.name}, {self.species}>'
